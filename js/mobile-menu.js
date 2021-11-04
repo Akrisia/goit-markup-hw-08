07761525924a0e7b1f7f49f3f0436127fe7f9ec6
Iryna S <irynaradkovska@gmail.com>
2021-11-04 05:57:40 +0200

    Редактирует скрипт для меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -7,8 +7,14 @@
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menu.addEventListener('click', removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+  }
+  function removeMenu(e) {
+    if (e.target === refs.menu) {
+      refs.menu.classList.add('is-hidden');
+    }
   }
 })();
